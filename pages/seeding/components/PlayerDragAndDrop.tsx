a914ad3b25ece0b899d1edefdf810f8019517911
Armando-CastVil <100313862+Armando-CastVil@users.noreply.github.com>
2022-09-27 20:24:36 -0700

    Armando/dragdropcomponent (#33)
    * add styling for draggable item
    
    * amendment to previous commit
    
    * setup for drag and drop component

---
+++ pages/seeding/components/PlayerDragAndDrop.tsx
@@ -0,0 +1,52 @@
+
+import { useState } from "react";
+import { Carpool } from "../types/seedingTypes";
+import Competitor from "../classes/Competitor";
+import AddToCarpoolButton from "./AddToCarpoolButton";
+import CarpoolDropDownMenu from "./CarpoolDropDownMenu";
+import styles from '/styles/Home.module.css'
+import DisplayProjectedPath from "./DisplayProjectedPath";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+
+export default function PlayerDragAndDrop()
+{
+    const [playerList, setPlayerList] = useState([]);
+
+    const grid = 8;
+
+    function onDragEnd(result:any) {
+        if (!result.destination) {
+          return;
+        }
+        if (result.destination.index === result.source.index) {
+            return;
+          }
+    }
+    const reorder = (list:Competitor[], startIndex:number, endIndex:number) => {
+        const result = Array.from(list);
+        const [removed] = result.splice(startIndex, 1);
+        result.splice(endIndex, 0, removed);
+      
+        return result;
+      };
+    const PlayerItem =`
+    width: 200px;
+    border: 1px solid grey;
+    margin-bottom: ${grid}px;
+    background-color: lightblue;
+    padding: ${grid}px;
+    `;
+ 
+    
+
+
+    return(
+        
+    <DragDropContext onDragEnd={onDragEnd}>
+      
+    </DragDropContext>
+
+    )
+        
+}
