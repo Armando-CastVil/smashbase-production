e38050a582298f27c25768a868d7044477dd6367
Armando Castaneda <armando.castvil@gmail.com>
2022-12-21 22:16:58 -0800

    added sol's styling to tables

--- pages/seeding/components/PlayerListDisplayStep.tsx
+++ pages/seeding/components/PlayerListDisplayStep.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import styles from '/styles/Seeding.module.css'
-import bracketGradient  from "/assets/seedingAppPics/bracketGradient.png"
 import Tournament from '../classes/Tournament';
 import Event from '../classes/TourneyEvent';
 import axios from 'axios';
@@ -11,7 +10,13 @@ import assignBracketIds from '../modules/assignBracketIds';
 import getMatchList from '../modules/getMatchList';
 import setProjectedPath from '../modules/setProjectedPath';
 import Competitor from '../classes/Competitor';
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import DynamicTable from '@atlaskit/dynamic-table';
+import { FC } from 'react';
+import { css, jsx } from '@emotion/react';
+
+
+
+import { ClassAttributes, OlHTMLAttributes, LegacyRef, ReactElement, JSXElementConstructor, ReactFragment, ReactPortal, LiHTMLAttributes } from 'react';
 interface props {
     page:number;
     setPage:(page:number) => void;
@@ -20,67 +25,124 @@ interface props {
     setPlayerList:(competitors:Competitor[]) => void;
     
 }
+
+interface NameWrapperProps {
+    children: React.ReactNode;
+  }
 export default function PlayerListDisplayStep({page,setPage,apiKey,playerList,setPlayerList}:props)
 {
-    //function that rearranges items when user stops dragging and drops
-    function handleOnDragEnd(result:any) {
-        if (!result.destination) return;
-        const items = Array.from(playerList);
-        const [reorderedItem] = items.splice(result.source.index, 1);
-        items.splice(result.destination.index, 0, reorderedItem);
-        setPlayerList(items);
-      }
+
+    var tempPlayerList:Competitor[]=playerList;
+    function swapCompetitors(firstPlayerIndex:number,secondPlayerIndex:number|undefined)
+        {
+            if(secondPlayerIndex==undefined)
+            {
+                console.log("swap function player list")
+                return tempPlayerList
+            }
+            else
+            {
+                var tempPlayer1:Competitor= JSON.parse(JSON.stringify(tempPlayerList[firstPlayerIndex]))
+                var tempPlayer2:Competitor= JSON.parse(JSON.stringify(tempPlayerList[secondPlayerIndex]))
+                tempPlayerList[secondPlayerIndex]=tempPlayer1
+                tempPlayerList[firstPlayerIndex]=tempPlayer2
+            }
+            console.log("temp player list after swapping:")
+            console.log(tempPlayerList)
+        }
+        function allOnClickEvents()
+        {
+            setPage(page + 1);
+            setPlayerList(tempPlayerList)
+        }
+    const NameWrapper: FC<NameWrapperProps> = ({ children }) => (
+        <span >{children}</span>
+      );
     
-  
+    const nameWrapperStyles = css({
+        display: 'flex',
+        alignItems: 'center',
+      });
+      const caption = 'Player List';
+
+      const createHead = (withWidth: boolean) => {
+        return {
+          cells: [
+            {
+              key: 'player',
+              content: 'Player',
+              isSortable: true,
+              width: withWidth ? 15 : undefined,
+            },
+            {
+              key: 'powerlvl',
+              content: 'Rating',
+              shouldTruncate: true,
+              isSortable: true,
+              width: withWidth ? 10 : undefined,
+            },
+          ],
+        };
+      };
+
+      const head = createHead(true);
+
+      const rows = tempPlayerList.map((player: Competitor, index: number) => ({
+        key: `row-${index}-${player.tag}`,
+        isHighlighted: false,
+        cells: [
+          {
+            key: createKey(player.tag),
+            content: (
+              <NameWrapper>
+                <a href="https://atlassian.design">{player.tag}</a>
+              </NameWrapper>
+            ),
+          },
+          {
+            key: player.smashggID,
+            content: player.rating,
+          },
+        ],
+      }));
+      
+   
+     
+      
+      
     return(
-        
-            <div >
-                <h1>ABSOLUTELY PUSHING PEE</h1>
-                <button
-            onClick={() => {
-                setPage(page + 1);
+        <div>
+             <button
+                    onClick={() => {
 
-            }}>
-            Next
-        </button>
-                
-                
-                <DragDropContext onDragEnd={handleOnDragEnd}>
-                    <Droppable droppableId="players">
-                        {(provided) => (
-                             <ol className="players" {...provided.droppableProps} ref={provided.innerRef}>
-                                {playerList.map(({smashggID, tag, rating}, index) => {
-                                    return (
-                                        <Draggable  key={smashggID} draggableId={smashggID.toString()} index={index}>
-                                            {(provided) => (
-                                                <li className={styles.list} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                                    
-                                                        <p>Tag: { tag }</p>
-                                                        <br></br>
-                                                        <p>Rating: {rating}</p>
-                                                    
-                                                </li>
-                                            )}
-                                        </Draggable>
-                                    );
-                                })}
-                                {provided.placeholder}
-                            </ol>
+                        setPage(page + 1);
+                        
+                    }}>
+                Next
+                </button>
+            <DynamicTable
+            head={head}
+            rows={rows}
+            rowsPerPage={10}
+            defaultPage={1}
+            loadingSpinnerSize="large"
+            isRankable={true}
+            onRankStart={(params) => console.log('onRankStart', params.index)}
+            onRankEnd={(params) => swapCompetitors(params.sourceIndex,params.destination?.index)}
+            />
 
-                        )}
-                    </Droppable>
-
-                </DragDropContext>
-            </div>
-            
-        
-
-                
+        </div>
            
   
     )
     
 }
 
-
-          
+function createKey(input: string) {
+    return input ? input.replace(/^(the|a|an)/, '').replace(/\s/g, '') : input;
+  }
+const nameWrapperStyles = css({
+    display: 'flex',
+    alignItems: 'center',
+    
+});
