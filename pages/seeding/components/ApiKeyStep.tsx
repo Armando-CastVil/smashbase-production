02e49468c6f6240e5cec14bccb6b8eb69b5e3e7c
Armando Castaneda <armando.castvil@gmail.com>
2023-01-14 20:19:11 -0800

    applied sol's styling to api key step page

--- pages/seeding/components/ApiKeyStep.tsx
+++ pages/seeding/components/ApiKeyStep.tsx
@@ -4,6 +4,9 @@ import styles from '/styles/Seeding.module.css'
 import bracketGradient  from "/assets/seedingAppPics/bracketGradient.png"
 import Tournament from '../classes/Tournament';
 import axios from 'axios';
+import { useState } from 'react';
+import SeedingFooter from './SeedingFooter';
+import { useAuthState } from 'react-firebase-hooks/auth';
 interface props {
     page:number;
     setPage:(page:number) => void;
@@ -14,8 +17,10 @@ interface props {
 }
 export default function ApiKeyStep({page,setPage,apiKey,setApiKey,setTournaments}:props)
 {
-   
+
     let hardCodedApiKey:string="06d2a6cd63f24966a65826634d8cc0e9"
+    
+
     
     
     const  handleSubmit = async () => {
@@ -26,13 +31,16 @@ export default function ApiKeyStep({page,setPage,apiKey,setApiKey,setTournaments
             await setTournaments(apiDataToTournaments(value))
             
         })
-     }
+    }
     
     
     return(
+
+
         <div>
             
-        
+            
+            
             <title>SmashBase Seeding Tool</title>
             <meta name="description" content=""></meta>
             <meta name="generator" content="Hugo 0.104.2"></meta>
@@ -40,33 +48,26 @@ export default function ApiKeyStep({page,setPage,apiKey,setApiKey,setTournaments
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous"></link>
             <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/sign-in/"></link>
             
-            
-            
+            <div className={styles.upperBody}>
+            <div className={styles.bodied}>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossOrigin="anonymous"></script>
                 <p className={styles.headingtext}> Paste your API key from ‎  <a href="www.start.gg">     Start.gg</a></p>
                 <div className={styles.vimembed}>
                     <iframe src="https://player.vimeo.com/video/766883703" width="640" height="360"  allow="autoplay; fullscreen" allowFullScreen></iframe>
                 </div>
                 <div className={styles.formsignin}>
-                    <form>
+                    <form >
                         <div className="form-floating textfieldtext">
                             <input type="password" className="form-control" id="floatingInput" placeholder="Enter your API key here" onChange={e => setApiKey(e.target.value)}></input>
                             <label> API Key </label>
                         </div>
                     </form>
                 </div>
-                <button
-                    onClick={() => {
-
-                        setPage(page + 1);
-                        handleSubmit();
-                    }}>
-                Next
-                </button>
-                <footer >absolutely pushing pee</footer>
-                
-                
-             
+                <SeedingFooter page={page} setPage={setPage} handleSubmit={handleSubmit} ></SeedingFooter>
+            
+            </div>
+            
+            </div>  
         
         </div>
     )
@@ -104,5 +105,16 @@ function APICall(apiKey:string)
             return data
         }
     )
+}
+
+var db:any;
+//saves the api key
+
+function getApiKey() {
+    if (typeof window !== 'undefined') {
+        return localStorage.getItem("seedingAppApiKey") || "";
+    } else {
+        return "";
+     }
 }
 
---
+++ pages/seeding/components/SeedingFooter.tsx
@@ -0,0 +1,61 @@
+import { ProgressIndicator } from '@atlaskit/progress-indicator';
+import Button from '@atlaskit/button/standard-button';
+import styles from '/styles/Seeding.module.css'
+import { useState } from 'react';
+
+interface props {
+    page:number;
+    setPage:(page:number) => void;
+    handleSubmit?:()=>void;
+}
+export default function SeedingFooter({page,setPage,handleSubmit}:props)
+{
+    const [values] = useState(['first', 'second', 'third','fourth','fifth']);
+
+    const handlePrev = () => {
+        setPage(page - 1);
+    };
+    const handleNext = () => {
+        if(handleSubmit!=undefined)
+        {
+            handleSubmit()
+        }
+        
+        setPage(page+ 1);
+      };
+    
+return(
+    <div style={{alignItems:"center"}} className={styles.seedAppFooter}>
+        
+                
+                    <div style={{display:"flex",  flexDirection:"row", alignItems:"center",marginRight:"auto",marginLeft:"auto"}}>
+
+                    <Button
+                        isDisabled={page === 0}
+                        onClick={handlePrev}
+                        appearance="primary"
+                    >
+                    Previous
+                    </Button>
+                   
+                    
+                    <div style={{padding:"0px 30px 0px"}}>
+                    <ProgressIndicator
+                    appearance="inverted"
+                    selectedIndex={page}
+                    values={values}
+                    />
+                    </div>
+                    
+                   
+                    <Button 
+                    onClick={handleNext}
+                    appearance="primary"
+                    >
+                    Next
+                    </Button>
+                    </div>
+                
+    </div>
+)
+}
