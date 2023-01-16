23a2eac43c694a710151e051c527458510b7cc85
Armando Castaneda <armando.castvil@gmail.com>
2023-01-15 22:24:43 -0800

    add styling to table

--- pages/seeding/components/TournamentDisplayStep.tsx
+++ pages/seeding/components/TournamentDisplayStep.tsx
@@ -4,6 +4,11 @@ import bracketGradient  from "/assets/seedingAppPics/bracketGradient.png"
 import Tournament from '../classes/Tournament';
 import axios from 'axios';
 import TourneyEvent from '../classes/TourneyEvent';
+import SeedingFooter from './SeedingFooter';
+import DynamicTable from '@atlaskit/dynamic-table';
+import { FC } from 'react';
+import { css, jsx } from '@emotion/react';
+import unixTimestampToDate from '../modules/unixTimestampToDate';
 interface props {
     page:number;
     setPage:(page:number) => void;
@@ -15,7 +20,7 @@ interface props {
 export default function TournamentDisplayStep({page,setPage,apiKey,tournaments,setEvents}:props)
 {
 
-    let hardCodedApiKey:string="06d2a6cd63f24966a65826634d8cc0e9"
+    
 
     async function callAllOnClickFunctions(hardCodedApiKey:string,slug:string)
     {
@@ -27,22 +32,95 @@ export default function TournamentDisplayStep({page,setPage,apiKey,tournaments,s
         
         setPage(page + 1);
     }
+
+    interface NameWrapperProps 
+    {
+        children: React.ReactNode;
+    }
+
+    const NameWrapper: FC<NameWrapperProps> = ({ children }) => (
+        <span >{children}</span>
+    );
+
+    
+    
+
+    
+
+    const createHead = (withWidth: boolean) => 
+    {
+        return {
+          cells: [
+            {
+              key: 'Tournament Name',
+              content: <a className={styles.tableHead}>Tournament Name</a>,
+              isSortable: true,
+              width: withWidth ? 70 : undefined,
+            },
+            {
+              key: 'Date',
+              content:<a className={styles.tableHead}>Date </a>,
+              shouldTruncate: true,
+              isSortable: true,
+              width: withWidth ? 100 : undefined,
+            },
+          ],
+        };
+    };
+
+    const head = createHead(true);
+
+    const rows = tournaments.map((tournament: any, index: number) => ({
+        key: `row-${index}-${tournament.name}`,
+        isHighlighted: false,
+        cells: [
+          {
+            key: createKey(tournament.name),
+            content: 
+              <NameWrapper>
+                <img src={tournament.imageURL} width={24} height={24} ></img>
+                <a  className={styles.tableRow}>{tournament.name}</a>
+                
+              </NameWrapper>
+            ,
+          },
+          {
+            key: tournament.startAt,
+            content:(<NameWrapper><a className={styles.tableRow}>{unixTimestampToDate(parseInt(tournament.startAt))}</a></NameWrapper>),
+          },
+        ],
+      }));
+    
+
+
+    
     return(
         <div>
-            <h1 className={styles.headingtext}>tournaments you are admin of:</h1>
-            <div>
-        
-            {tournaments.map((t: Tournament) =>
-            {
-            return <>
-            <div onClick={() =>callAllOnClickFunctions(hardCodedApiKey,t.slug!) } key={t.name} className={styles.tournaments} >
-                <h3  >{t.name}</h3> <img src={t.imageURL} width={100} height={100} ></img>
-                <h6 >Date: {t.startAt}</h6>
-                <h6  id="tourneyLink"><a href={"smash.gg"+t.url} target="_blank" rel="noopener noreferrer">Link to Tournament</a> </h6>
+            <div className={styles.upperBody}>
+                <div className={styles.bodied}>
+                
+                    <h1 className={styles.headingtext}>Tournaments you are admin of:</h1>
+                
+                
+                    
+                    <div className={styles.tourneyTable}>
+                    <DynamicTable
+                        
+                        head={head}
+                        rows={rows}
+                        rowsPerPage={10}
+                        defaultPage={1}
+            
+                        loadingSpinnerSize="large"
+                        isRankable={true}
+                    />
+
+                    </div>
+                    <SeedingFooter page={page} setPage={setPage}  ></SeedingFooter>
+
+                
                 </div>
-                <br/>
-            </>
-            })}
+                
             </div>
            
         </div>
@@ -78,3 +156,20 @@ function APICall(apiKey:string,slug:string)
     )
 }
 
+function createKey(input: string) {
+    return input ? input.replace(/^(the|a|an)/, '').replace(/\s/g, '') : input;
+}
+
+/*
+   {tournaments.map((t: Tournament) =>
+                        {
+                            return <>
+                            <div onClick={() =>callAllOnClickFunctions(apiKey!,t.slug!) } key={t.name} className={styles.tournaments} >
+                            <h3  >{t.name}</h3> <img src={t.imageURL} width={100} height={100} ></img>
+                            <h6 >Date: {t.startAt}</h6>
+                            <h6  id="tourneyLink"><a href={"smash.gg"+t.url} target="_blank" rel="noopener noreferrer">Link to Tournament</a> </h6>
+                            </div>
+                            <br/>
+                            </>
+                    })}
+*/
---
+++ pages/seeding/modules/unixTimestampToDate.ts
@@ -0,0 +1,13 @@
+//This functions converts unix timestamps to day/month/year format
+export default function unixTimestampToDate(UNIX_timestamp: number){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = date + ' ' + month + ' ' + year+" "  ;
+    return time;
+  }
