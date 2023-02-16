b3e76b0c062b51bd843e8e4b3e00289e22734db2
Armando Castaneda <armando.castvil@gmail.com>
2023-02-15 16:38:45 -0800

    add inline messages after check for valid api key

--- pages/seeding/components/ApiKeyStep.tsx
+++ pages/seeding/components/ApiKeyStep.tsx
@@ -12,6 +12,9 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from '../../utility/firebaseConfig';
 import queryFirebase from '../modules/queryFirebase';
+import InlineMessage from '@atlaskit/inline-message';
+
+
 
 //Initialize Firebase configuration
 export const app = initializeApp(firebaseConfig);
@@ -29,47 +32,156 @@ interface props {
 
 export default function ApiKeyStep({page,setPage,apiKey,setApiKey,setTournaments}:props)
 {
-
-    
-    onAuthStateChanged(auth, (user) => {
-        fillInApiKey(user)
-    });
-    function fillInApiKey(user:any) {
-        //only look for api key if they are logged in and its not already there
-        if (!(user && (apiKey == null || apiKey == ""))) return;
-        const uid = user.uid;
-        //pull it from firebase
-        queryFirebase("apiKeys/"+uid,0).then((value) => {
-            //check firebase first
-            if(value != null) setApiKey(value);
-            //then check local storage
-            else if (typeof window !== 'undefined') {
-                setApiKey(localStorage.getItem("seedingAppApiKey") || "");
-            } else {
-                //if its in neither place, just set it to empty string
-                setApiKey("");
-            }
-        });
-    }
-    
-
-    
-    
-    const  handleSubmit = async () => {
-        if(apiKey!=undefined)
-        {
-            await APICall(apiKey).then(async (value)=>
-            {
-              
-                await setTournaments(apiDataToTournaments(value))
-                
-            })
+    //value that verifies if key is valid
+    //0 is for api key that hasnt been input
+    //1 is for undefined api key
+    //2 is for not valid api key
+    //3 is for valid api key
+    const [keyStatus,setKeyStatus]=useState<number>(0)
+    //because a state change triggers a re-render, we cannot use one to go to next page w/o submitting twice
+    //so we will use another variable instead of a state for that
+    let keyIsGood=false
+//fills in api key 
+onAuthStateChanged(auth, (user) => 
+{
+    fillInApiKey(user)
+});
+function fillInApiKey(user:any) 
+{
+    //only look for api key if they are logged in and its not already there
+    if (!(user && (apiKey == null || apiKey == ""))) return;
+    const uid = user.uid;
+    //pull it from firebase
+    queryFirebase("apiKeys/"+uid,0).then((value) => {
+        //check firebase first
+        if(value != null) setApiKey(value);
+        //then check local storage
+        else if (typeof window !== 'undefined') {
+            setApiKey(localStorage.getItem("seedingAppApiKey") || "");
+        } else {
+            //if its in neither place, just set it to empty string
+            setApiKey("");
         }
-       
+    });
+}
+    
+function handleKeyPress(event: { key: string; preventDefault: () => void; }) 
+{
+     // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    handleSubmit()
+  }
+    
+}
+    
+//this function handles the data returned by the api call
+function apiDataToTournaments(apiData:any)
+{
+    //tournaments are stored here
+    let tournamentArray:Tournament[]=[]
+    //data is undefined if an invalid key was provided by user
+    if(apiData.data==undefined)
+    {
+        setKeyStatus(2)
+        return tournamentArray
     }
+    //if the key is valid but the length is 0, then the user is not an admin of any tournaments
+    else if(apiData.data.currentUser.tournaments.nodes.length==0)
+    {
+        setKeyStatus(3)
+        return tournamentArray
+    }
+    else
+    {
+        //if we made it to here it is because there is data, this is then processed
+        for(let i=0;i<apiData.data.currentUser.tournaments.nodes.length;i++)
+        {
+        
+            let name:string=apiData.data.currentUser.tournaments.nodes[i].name
+            let city:string=apiData.data.currentUser.tournaments.nodes[i].city
+            let url:string=apiData.data.currentUser.tournaments.nodes[i].url
+            let slug:string=apiData.data.currentUser.tournaments.nodes[i].slug
+            let startAt:number=apiData.data.currentUser.tournaments.nodes[i].startAt
+            let imageURL=undefined
+            if(apiData.data.currentUser.tournaments.nodes[i].images.length!=0)
+            {
+                imageURL=apiData.data.currentUser.tournaments.nodes[i].images[0].url
+            }
+            
+            
+            let tempTournament=new Tournament(name,city,url,slug,startAt,imageURL)
+            tournamentArray.push(tempTournament)
+        }
+
+    }
+   
+    return tournamentArray;
+}
+
+//this function handles the user's submitted api key  
+const  handleSubmit = async () => 
+{
     
     
-    return(
+    
+    
+    //if no api key is entered by the user, they are made aware and function is exited
+    if(apiKey==undefined || apiKey.length==0)
+    {
+        setKeyStatus(1)
+        return
+    }
+
+    //if user does enter a value, we check if it returns tournaments from the api call
+    //either the api key is invalid, or the user is not an admin of any tournaments, this
+    //is handled in the apiDataToTournaments function
+    if(apiKey!=undefined)
+    {
+        await APICall(apiKey).then(async (value)=>
+        {
+            console.log(value)
+            console.log(value.length)
+            //we make the api call with the user's provided api key
+            setTournaments(apiDataToTournaments(value)!)
+            //if it doesnt return a tournament, either api key is invalid or they are not an admin of any tournaments
+            //so we return
+            if(value.data==undefined)
+            {
+                console.log("VALUE IS NULL")
+                return
+            }
+            //tournaments are stored in the value variable, if it is not empty then it means there are
+            //tournaments and we can proceed
+            else
+            {
+                console.log("KEY IS GOOD")
+                keyIsGood=true
+                
+            }    
+
+        })
+
+        //only goes to the next page if the key is valid
+        if(keyIsGood)
+        {
+            setPage(page+ 1);
+        }
+        //return if it's not valid
+        else return
+    }
+
+    
+       
+}
+
+
+
+
+    
+return(
 
 
         <div>
@@ -93,11 +205,66 @@ export default function ApiKeyStep({page,setPage,apiKey,setApiKey,setTournaments
                 <div className={styles.formsignin}>
                     <form >
                         <div className="form-floating textfieldtext">
-                            <input type="password" className="form-control" id="floatingInput" placeholder="Enter your API key here" value={apiKey} onChange={e => setApiKey(e.target.value)}></input>
-                            <label> API Key </label>
+                            <input type="password" className="form-control" id="floatingInput" placeholder="Enter your API key here" value={apiKey} onKeyDown={e=>handleKeyPress(e)}  onChange={e => setApiKey(e.target.value)}></input>
+                            <label> Enter your API Key here </label>
                         </div>
                     </form>
                 </div>
+                
+                {keyStatus==0?
+                    <p></p>:
+                    keyStatus==1?
+                    <InlineMessage
+                    appearance="error"
+                    iconLabel="Error! API Key form is empty."
+                    secondaryText="API Key form is empty."
+                    >
+                    <p>Please enter your API Key</p>
+                    </InlineMessage>
+                    :
+                    keyStatus==2?
+                    <InlineMessage
+                    appearance="error"
+                    iconLabel="Error! API Key form is not valid."
+                    secondaryText="API Key form is not valid."
+                    >
+                    <p>Please enter a valid API Key</p>
+                    </InlineMessage>
+                    :
+                    keyStatus==3?
+                    <InlineMessage
+                    appearance="error"
+                    iconLabel="Error! No tournaments were found under this API Key user"
+                    secondaryText="No tournaments were found under this API Key user."
+                    >
+                    <p>Please make sure you are an admin for the tournament you want to seed</p>
+                    </InlineMessage>
+                    :
+                    <InlineMessage
+                    appearance="confirmation"
+                    secondaryText="Valid API Key!"
+                    >
+                    <p>Valid API Key!</p>
+                    </InlineMessage>
+                }
+                {keyIsGood?
+                <InlineMessage
+                appearance="confirmation"
+                secondaryText="Valid API Key!"
+                >
+                <p>Valid API Key!</p>
+                </InlineMessage>:
+                <p></p>
+
+
+                }
+                
+                
+                
+                
+                
+                
+                
                 <SeedingFooter page={page} setPage={setPage} handleSubmit={handleSubmit} ></SeedingFooter>
             
             </div>
@@ -107,30 +274,10 @@ export default function ApiKeyStep({page,setPage,apiKey,setApiKey,setTournaments
         </div>
     )
 }
-function apiDataToTournaments(apiData:any)
-{
-    let tournamentArray:Tournament[]=[]
-    for(let i=0;i<apiData.data.currentUser.tournaments.nodes.length;i++)
-    {
-    
-        let name:string=apiData.data.currentUser.tournaments.nodes[i].name
-        let city:string=apiData.data.currentUser.tournaments.nodes[i].city
-        let url:string=apiData.data.currentUser.tournaments.nodes[i].url
-        let slug:string=apiData.data.currentUser.tournaments.nodes[i].slug
-        let startAt:number=apiData.data.currentUser.tournaments.nodes[i].startAt
-        let imageURL=undefined
-        if(apiData.data.currentUser.tournaments.nodes[i].images.length!=0)
-        {
-            imageURL=apiData.data.currentUser.tournaments.nodes[i].images[0].url
-        }
-        
-        
-        let tempTournament=new Tournament(name,city,url,slug,startAt,imageURL)
-        tournamentArray.push(tempTournament)
-    }
-    return tournamentArray;
-}
-function APICall(apiKey:string)
+
+
+//function for api call
+async function APICall(apiKey:string)
 {
     
     //API call
@@ -142,13 +289,10 @@ function APICall(apiKey:string)
     )
 }
 
-var db:any;
-//saves the api key
-function saveApiKey(apiKey:string|undefined) {
-   
-    if(!db) db = getDatabase();
-    set(ref(db,"apiKeys/"+auth.currentUser!.uid), apiKey);
-}
+
+
+
+
 
 
 
