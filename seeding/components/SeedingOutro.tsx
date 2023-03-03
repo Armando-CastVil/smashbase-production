6995d0410773fc1db4a0e75f093a160f5517eef7
solomonsolaries <abas.solomon@gmail.com>
2023-03-03 15:47:31 -0800

    Updated seeding outro
    Seeding

--- seeding/components/SeedingOutro.tsx
+++ seeding/components/SeedingOutro.tsx
@@ -11,11 +11,11 @@ export default function SeedingOutro()
                     <div className={styles.headingDiv}>
                     <h1 className={styles.seedHeading}>Seeding Complete!</h1>
                     <br></br>
-                    <p className={styles.seedSubheading}>If there are any issues with the seeding, please leave us feedback in our Discord</p>
+                    <p className={styles.seedSubheading}>If there are any issues with the seeding, please leave us feedback in our&nbsp;<a href="discord.gg/yyjMEHmBtg"> Discord </a></p>
                     </div>
                     <div className={styles.seedAppFooter}>
                         <div style={{display:"flex",  flexDirection:"row", alignItems:"center",marginRight:"auto",marginLeft:"auto",gap:"5%"}}>
-                            <Button appearance="primary">Return to Home</Button>
+                            <Button appearance="primary"> <a href="beta.smashbase.gg"> Return to Home </a> </Button>
                             <Button appearance="primary">Go to Start.GG page</Button>
                         </div>
                     </div>
