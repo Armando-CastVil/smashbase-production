3dc9ac484dc196e115487628543902e572eb5bf8
solomonsolaries <abas.solomon@gmail.com>
2023-09-29 05:00:01 -0700

    Added sep stat icons, fixed settings bg bug

--- seeding/components/FinalStep/subcomponents/finalStepHeading.tsx
+++ seeding/components/FinalStep/subcomponents/finalStepHeading.tsx
@@ -1,9 +1,47 @@
-import stepStyles from "../../../../styles/PlayerListDisplayStep.module.css"
-export default function finalStepHeading() 
-{
+import stepStyles from "../../../../styles/FinalStep.module.css";
+import locationSep from "assets/seedingAppPics/locationseparation.png";
+import setSep from "assets/seedingAppPics/setseparation.png";
+import carpoolSep from "assets/seedingAppPics/carpoolseparation.png";
+import Image from "next/image";
+
+export default function finalStepHeading() {
   return (
-    <div className={stepStyles.heading}>
-      <p>Check and submit seeding</p>
+    <div className={stepStyles.flexHeader}>
+      <div className={stepStyles.heading}>
+        <p>Check and submit seeding</p>
+      </div>
+      <div className={stepStyles.sepStatsContainer}>
+        <div className={stepStyles.statContainer}>
+          <p>
+            9
+          </p>
+          <Image
+            src={locationSep}
+            className={stepStyles.sepStatIcon}
+            alt="verified check mark"
+          />
+        </div>
+        <div className={stepStyles.statContainer}>
+          <p>
+            2
+          </p>
+          <Image
+            src={setSep}
+            className={stepStyles.sepStatIcon}
+            alt="verified check mark"
+          />
+        </div>
+        <div className={stepStyles.statContainer}>
+          <p>
+            3
+          </p>
+          <Image
+            src={carpoolSep}
+            className={stepStyles.sepStatIcon}
+            alt="verified check mark"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
