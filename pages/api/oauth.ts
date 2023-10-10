27eb56cedd8de79f108f3e6806657ba4849d4f73
Armando Castaneda <armando.castvil@gmail.com>
2023-10-09 20:58:48 -0700

    separated some functions

--- pages/api/oauth.ts
+++ pages/api/oauth.ts
@@ -1,23 +1,27 @@
-import axios from "axios";
+
 
 export {}
 
 // Import necessary libraries and modules
+// Import necessary libraries and modules
 const express = require('express');
+const axios = require('axios'); // Import Axios for making HTTP requests
+
+// Create an Express application
+const app = express();
 
 
 // Define your OAuth client credentials and redirect URI
  // OAuth configuration
  const CLIENT_ID = 51;
  const CLIENT_SECRET = '7b685b7d111ec191220d31fb3779fa158c621bba5175375855c47ff4e1f9d46d';
- const REDIRECT_URI = 'https://aerodusk.smashbase.gg/api/auth/callback/oauth';
+ const REDIRECT_URI = 'https://aerodusk.smashbase.gg/api/oauth';
 
 // Create an Express router
-const router = express.Router();
-
+const router = app.Router();
 
 // Define a route handler for '/oauth' endpoint
-router.get("/api/auth/callback/oauth", async (req:any, res:any) => {
+app.get("/api/oauth", async (req:any, res:any) => {
   console.log("so back")
   try {
     // Extract the 'code' query parameter from the request
@@ -72,6 +76,5 @@ router.get("/api/auth/callback/oauth", async (req:any, res:any) => {
   }
 });
 
-// Export the router to use in your Express application
-module.exports = router;
+
 
