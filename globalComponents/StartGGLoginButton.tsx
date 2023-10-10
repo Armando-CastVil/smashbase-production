3743bde032c25be65a9b45d4991f0a3a6f4e093c
Armando Castaneda <armando.castvil@gmail.com>
2023-10-09 18:24:19 -0700

    removed redirect uri from url

--- globalComponents/StartGGLoginButton.tsx
+++ globalComponents/StartGGLoginButton.tsx
@@ -3,13 +3,12 @@
 
 const CLIENT_ID = 51;
 const CLIENT_SECRET = '7b685b7d111ec191220d31fb3779fa158c621bba5175375855c47ff4e1f9d46d';
-const REDIRECT_URI = 'https://aerodusk.smashbase.gg/oauth';
+const REDIRECT_URI = 'https://aerodusk.smashbase.gg/api/auth/callback/oauth';
 const scopes = 'user.identity';
 export const StartGGLoginButton = () => {
     const handleLoginClick = () => {
       // Redirect the user to the start.gg authorization URL
-      
-      window.location.href = `http://start.gg/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&scope=user.identity&redirect_uri=${REDIRECT_URI}&client_secret=${CLIENT_SECRET}`;
+      window.location.href = `http://start.gg/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&scope=user.identity&client_secret=${CLIENT_SECRET}`;
     };
   
     return (
