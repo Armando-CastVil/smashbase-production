b055df0e7db209ea1a7c2f7230e4c4d3aace9ade
Armando Castaneda <armando.castvil@gmail.com>
2023-10-10 15:47:10 -0700

    applying chatgpt suggestion

--- pages/api/oauth.ts
+++ pages/api/oauth.ts
@@ -1,52 +1,37 @@
+// pages/api/oauth.js
 
-
-export { }
-
-// Import necessary libraries and modules
-// Import necessary libraries and modules
-const express = require('express');
-const axios = require('axios'); // Import Axios for making HTTP requests
-
-// Create an Express application
-const app = express();
-
+import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 // Define your OAuth client credentials and redirect URI
-// OAuth configuration
 const CLIENT_ID = 51;
 const CLIENT_SECRET = '21ed6b22f88c7b4f01fc8c1caef94837098e950d903456a1d0b1aa4f4eee4617';
 const REDIRECT_URI = 'https://aerodusk.smashbase.gg/api/oauth';
 
-
-// Define a route handler for '/oauth' endpoint
-app.get("/api/oauth", async (req: any, res: any) => {
-  console.log("so back")
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     // Extract the 'code' query parameter from the request
-    const { query: { code } } = req;
-    console.log(code)
-
+    const { code } = req.query as { code: string };
 
     // Check if the 'code' parameter is missing
     if (!code) {
-      return res.status(400).json({ 'error': 'OAuth login failed. Please restart the flow. Error: missing code' });
+      return res.status(400).json({ error: 'OAuth login failed. Please restart the flow. Error: missing code' });
     }
 
     // Create the data object with OAuth parameters for token exchange
     const data = {
-      'client_id': CLIENT_ID,
-      'client_secret': CLIENT_SECRET,
-      'grant_type': 'authorization_code',
-      'code': code,
-      'redirect_uri': REDIRECT_URI,
-      'scope': "user.identity"
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      grant_type: 'authorization_code',
+      code:code,
+      redirect_uri: REDIRECT_URI,
+      scope: 'user.identity',
     };
 
     // Define headers for the HTTP POST request
     const headers = {
       'Content-Type': 'application/json',
     };
-
 
     // Make a POST request to exchange the authorization code for tokens
     const response = await axios.post('https://api.start.gg/oauth/access_token', data, {
@@ -64,15 +49,15 @@ app.get("/api/oauth", async (req: any, res: any) => {
     console.log('Token Expires In:', expiresIn);
 
     // Send a response to the client to close the OAuth flow
-    res.redirect('/');
-
+    res.status(200).send(`
+      <script>
+        window.close();
+      </script>
+    `);
   } catch (error) {
     console.error(error);
 
     // Handle errors by sending an error response
     res.status(500).send('Error completing OAuth login.');
   }
-});
-
-
-
+};
