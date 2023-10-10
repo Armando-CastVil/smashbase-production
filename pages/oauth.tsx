763bc9860f4710e2e00ffa95da58020e7b731379
Armando Castaneda <armando.castvil@gmail.com>
2023-10-09 18:48:01 -0700

    Extracting code

--- pages/oauth.tsx
+++ pages/oauth.tsx
@@ -1,11 +1,18 @@
 export {}
-import type { NextPage } from 'next'
+import { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import globalStyles from '../styles/GlobalSeedingStyles.module.css'
 
 const Oauth: NextPage = () => {
+    const router = useRouter()
+    const { code } = router.query
+
+    // Now, 'code' contains the value of the 'code' query parameter
+
     return (
         <div className={globalStyles.body}>
             <h1>OAUTH PAGE</h1>
+            <p>Code: {code}</p>
         </div>
     )
 }
