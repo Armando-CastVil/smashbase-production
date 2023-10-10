93c26bad19a48da3b9b4b369382522567358635b
Armando Castaneda <armando.castvil@gmail.com>
2023-10-09 18:37:48 -0700

    made oauth page

--- globalComponents/StartGGLoginButton.tsx
+++ globalComponents/StartGGLoginButton.tsx
@@ -3,7 +3,7 @@
 
 const CLIENT_ID = 51;
 const CLIENT_SECRET = '7b685b7d111ec191220d31fb3779fa158c621bba5175375855c47ff4e1f9d46d';
-const REDIRECT_URI = 'https://aerodusk.smashbase.gg/api/auth/callback/oauth';
+const REDIRECT_URI = 'https://aerodusk.smashbase.gg/oauth';
 const scopes = 'user.identity';
 export const StartGGLoginButton = () => {
     const handleLoginClick = () => {
