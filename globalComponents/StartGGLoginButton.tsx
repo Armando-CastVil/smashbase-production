9e28f43368a2819e033c909dab6a95500ac51e6b
Armando Castaneda <armando.castvil@gmail.com>
2023-10-10 15:58:38 -0700

    redirect uri no variable

--- globalComponents/StartGGLoginButton.tsx
+++ globalComponents/StartGGLoginButton.tsx
@@ -3,12 +3,12 @@
 
 const CLIENT_ID = 51;
 const CLIENT_SECRET = '21ed6b22f88c7b4f01fc8c1caef94837098e950d903456a1d0b1aa4f4eee4617';
-const REDIRECT_URI = 'https://aerodusk.smashbase.gg/api/oauth';
+const REDIRECT_URI = "https://aerodusk.smashbase.gg/api/oauth";
 const scopes = 'user.identity';
 export const StartGGLoginButton = () => {
     const handleLoginClick = () => {
       // Redirect the user to the start.gg authorization URL
-      window.location.href = `http://start.gg/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&scope=user.identity&client_secret=${CLIENT_SECRET}`;
+      window.location.href = `http://api.start.gg/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&scope=${scopes}&redirect_uri=&client_secret=${CLIENT_SECRET}`;
     };
   
     return (
