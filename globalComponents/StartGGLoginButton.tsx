a3091ca3b8aaffdec1514df3fb733316eaab18a9
Armando Castaneda <armando.castvil@gmail.com>
2023-10-18 17:47:34 -0700

    update callback url

--- globalComponents/StartGGLoginButton.tsx
+++ globalComponents/StartGGLoginButton.tsx
@@ -3,7 +3,7 @@
 
 const CLIENT_ID = 51;
 const CLIENT_SECRET = '21ed6b22f88c7b4f01fc8c1caef94837098e950d903456a1d0b1aa4f4eee4617';
-const REDIRECT_URI = "https://aerodusk.smashbase.gg/api/oauth";
+const REDIRECT_URI = "https://localhost:3000/oauth";
 const scopes = 'user.identity';
 export const StartGGLoginButton = () => {
     const handleLoginClick = () => {
