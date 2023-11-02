f0eff883e5a866bf1697812095db46d093441806
Armando Castaneda <armando.castvil@gmail.com>
2023-11-01 23:00:17 -0700

    add client secret parameter

--- globalComponents/StartGGLoginButton.tsx
+++ globalComponents/StartGGLoginButton.tsx
@@ -6,7 +6,7 @@ import { User } from "./modules/globalTypes";
 
 const CLIENT_ID = 51;
 const CLIENT_SECRET = '21ed6b22f88c7b4f01fc8c1caef94837098e950d903456a1d0b1aa4f4eee4617';
-const REDIRECT_URI = "https://aerodusk.smashbase.gg/oauth";
+const REDIRECT_URI = "http://localhost:3000/oauth";
 const scopes = 'user.identity';
 export const StartGGLoginButton = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -23,7 +23,7 @@ export const StartGGLoginButton = () => {
     }, []);
   
     const handleLoginClick = () => {
-      window.location.href = `http://start.gg/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&scope=user.identity%20user.email&redirect_uri=${REDIRECT_URI}`;
+      window.location.href = `http://start.gg/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&scope=user.identity%20user.email&redirect_uri=${REDIRECT_URI}&client_secret=${CLIENT_SECRET}`;
     };
   
     const handleLogoutClick = () => {
