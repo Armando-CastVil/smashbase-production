605d196a8cd1cf8f249875c4a210716e1a1abc69
Armando Castaneda <armando.castvil@gmail.com>
2023-11-06 19:25:16 -0800

    add api key property to seeding intro props

--- seeding/components/SeedingIntro/modules/seedingIntroProps.ts
+++ seeding/components/SeedingIntro/modules/seedingIntroProps.ts
@@ -1,7 +1,10 @@
+import { Tournament } from "../../../definitions/seedingTypes";
+
 export default interface SeedingIntroProps {
     page: number;
     setPage: (page: number) => void;
     setStartTime: (startTime: number) => void;
+    apiKey:string|undefined;
     setApiKey: (apiKey: string|undefined) => void;
+    setTournaments: (tournaments: Tournament[]) => void;
   }
-  
