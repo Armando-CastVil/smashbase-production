594d528bb28c0678870d2af35f46d0fa03f8a0f9
Armando Castaneda <armando.castvil@gmail.com>
2024-04-06 09:26:37 -0700

    testing old sorting function

--- seeding/components/EventDisplayStep/modules/sortByRating.ts
+++ seeding/components/EventDisplayStep/modules/sortByRating.ts
@@ -1,6 +1,9 @@
 import { Player } from "../../../definitions/seedingTypes";
 export default function sortByRating(entryList:Player[]):Player[]
 {
+console.log("this is the og sorting function")
 const sortedList=entryList.sort((entry1,entry2)=>(entry1.rating<entry2.rating)? 1 :(entry1.rating>entry2.rating) ?-1:0);
+console.log("og list length:"+ entryList)
+console.log("after list length:"+ sortedList)
 return sortedList
 }
