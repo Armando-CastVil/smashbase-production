966eed72f261754dd81167c1195609f0fa49d6ad
Armando Castaneda <armando.castvil@gmail.com>
2024-04-06 09:18:40 -0700

    testing old sorting function

--- seeding/components/EventDisplayStep/modules/sortByRating.ts
+++ seeding/components/EventDisplayStep/modules/sortByRating.ts
@@ -1,14 +1,6 @@
 import { Player } from "../../../definitions/seedingTypes";
-
-export default function sortByRating(entryList: Player[]): Player[] {
-    console.log("og list length"+ entryList.length)
-  // Create a copy of the original array
-  const sortedList = [...entryList].sort((entry1, entry2) => entry2.rating - entry1.rating);
-  console.log("sorted list length"+ sortedList.length)
-  // Alternatively, you can use the slice() method to create a copy
-  // const sortedList = entryList.slice().sort((entry1, entry2) => entry2.rating - entry1.rating);
-
-  
-  return sortedList;
-
+export default function sortByRating(entryList:Player[]):Player[]
+{
+const sortedList=entryList.sort((entry1,entry2)=>(entry1.rating<entry2.rating)? 1 :(entry1.rating>entry2.rating) ?-1:0);
+return sortedList
 }
