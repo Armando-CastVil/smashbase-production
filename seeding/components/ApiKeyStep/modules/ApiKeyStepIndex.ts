0dcf28a56322c39c1ac06ecb4aec46cfd02767a4
Armando Castaneda <armando.castvil@gmail.com>
2024-10-08 23:37:21 -0700

    made apikeystepprops default type

--- seeding/components/ApiKeyStep/modules/ApiKeyStepIndex.ts
+++ seeding/components/ApiKeyStep/modules/ApiKeyStepIndex.ts
@@ -1,20 +1,23 @@
-//This file imports and exports all the necessary pieces at once 
-//in order to avoid having a big list of import statements at the top of components
+// This file imports and exports all the necessary pieces at once 
+// to avoid having a big list of import statements at the top of components
 
+// interfaces (marked as type-only)
+import type ApiKeyStepProps from "./APiKeyStepProps";
+export type { ApiKeyStepProps as default };
 
-
-//interfaces
-import ApiKeyStepProps from "./APiKeyStepProps";
-export default ApiKeyStepProps
-
-//subcomponents
+// subcomponents
 import ErrorMessage from "../subcomponents/ErrorMessage";
-export {ErrorMessage}
 import EmbeddedVideo from "../subcomponents/EmbededVideo";
-export {EmbeddedVideo}
 import ApiKeyForm from "../subcomponents/ApiKeyForm";
-export {ApiKeyForm}
 import Heading from "../subcomponents/Heading";
-export {Heading}
+
+// enums
 import ErrorCode from "./enums";
-export {ErrorCode}
+
+export {
+  ErrorMessage,
+  EmbeddedVideo,
+  ApiKeyForm,
+  Heading,
+  ErrorCode
+};
